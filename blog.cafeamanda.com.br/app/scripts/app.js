bf57ddc476208ae49fe01c53af13bce6938278b7
cafeamanda <amandadearaujo0@gmail.com>
2016-08-05 00:28:30 -0300

    Blog & Pre-FAQ no cafeamanda

--- blog.cafeamanda.com.br/app/scripts/app.js
+++ blog.cafeamanda.com.br/app/scripts/app.js
@@ -1,49 +1,75 @@
-var app = angular.module('tutorialWebApp', ['ngRoute']);
+'use strict';
 
-/**
-*  Routes
-*/
-app.config(['$routeProvider', function ($routeProvider) {
-  $routeProvider
+angular.module('blogApp', ['ui.router'])
+.config(function($stateProvider, $urlRouterProvider) {
+  $stateProvider
     // Home
-    .when("/", {
-      templateUrl: "partials/home.html", <span class="highlight">
-        controller: "PageCtrl"
-      </span>
+    .state('app', {
+      url:'/',
+      views: {
+        'header': {
+          templateUrl: 'app/templates/header.html'
+        },
+        'content': {
+          templateUrl: 'app/partials/home.html',
+          controller: 'PageCtrl'
+        },
+        'sidebar': {
+          templateUrl: 'app/templates/sidebar.html'
+        },
+        'footer': {
+          templateUrl: 'app/templates/footer.html'
+        }
+      }
     })
 
-    // Pages
-    .when("/about", {
-      templateUrl: "partials/about.html",
-      <span class="highlight">
-        controller: "PageCtrl"
-      </span>
+    .state('app.about', {
+      url: 'about',
+      views: {
+        'content@': {
+          templateUrl: 'app/partials/about.html',
+          controller: 'PageCtrl'
+        }
+      }
     })
 
-    .when("/faq", {
-      templateUrl: "partials/faq.html",
-      <span class="highlight">
-        controller: "PageCtrl"
-      </span>
+    .state('app.blog', {
+      url: 'blog',
+      views: {
+        'content@': {
+          templateUrl: 'app/partials/blog.html',
+          controller: 'BlogCtrl'
+        }
+      }
     })
 
-    // Blog
-    .when("/blog", {
-      templateUrl: "partials/blog.html", <span class="highlight">controller: "BlogCtrl"</span>})
-
-    .when("/blog/post", {
-      templateUrl: "partials/blog/blog_item.html",
-      <span class="highlight">
-        controller: "BlogCtrl"
-      </span>
+    .state('app.post01', {
+      url:'pensando-o-seu-site',
+      views: {
+        'content@': {
+          templateUrl: 'app/partials/blog/post01.html'
+        }
+      }
     })
 
-    // Else 404
-    .otherwise("/404", {
-      templateUrl: "partials/404.html",
-      <span class="highlight">
-        controller: "PageCtrl"
-      </span>
-    });
+    .state('app.post02', {
+      url:'10-excelentes-recursos-de-engajamento-para-o-seu-site',
+      views: {
+        'content@': {
+          templateUrl: 'app/partials/blog/post02.html'
+        }
+      }
+    })
 
-}]);
+    .state('app.post03', {
+      url:'sobre-o-sobre-nos',
+      views: {
+        'content@': {
+          templateUrl: 'app/partials/blog/post03.html'
+        }
+      }
+    })
+
+    $urlRouterProvider.otherwise('/');
+
+});
